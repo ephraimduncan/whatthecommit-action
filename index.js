71a631699093c5b9f50a1232cadf5570774c680b
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-18 23:30:34 +0000

    build: index.js

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-const core = require('@actions/core');
 const axios = require('axios');
 const { execSync } = require('child_process');
 const log = (msg) => console.log(`\n${msg}`);
-// const exit = (msg) => {
-//   console.error(`\n${msg}`);
-//   process.exit(1);
-// };
-const run = (cmd, cwd) =>
-  execSync(cmd, { encoding: 'utf8', stdio: 'inherit', cwd });
+const run = (cmd, cwd) => execSync(cmd, { encoding: 'utf8', stdio: 'inherit', cwd });
 const rootDir = '.';
-// Get New Commit Message
-// Edit Commit on everypush with The New Commit Message
-// Push (force) the commit. ..If err, add git config
 
 async function __() {
   const _ = await axios.get('http://whatthecommit.com/index.txt');
   log('Creating New Commit....');
-  run(`git commit --amend -m ${_.data}`, rootDir);
+  run(`git commit --amend -m ${JSON.stringify(_.data).replace(/(\r\n|\n|\r)/gm, '')}`, rootDir);
 
-  // log('Pushing New Commit....');
-  // run(`git push --force`, rootDir);
+  log('Pushing New Commit....');
+  run(`git push --force ${process.env.GITHUB_REPOSITORY.split('/')[1]} ${process.env.GITHUB_REF.split('/')[2]}`, rootDir);
 }
 
 try {
   __();
 } catch (error) {
-  core.setFailed(error.message);
+  throw new Error(error);
 }
