c770184a3a0826e9bc267a87ea3d6cabc0f91664
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-19 11:31:27 +0000

    I removed add .

--- dist/index.js
+++ dist/index.js
@@ -8372,7 +8372,7 @@ async function ____() {
   const _____ = await _.get('http://whatthecommit.com/index.txt');
   const ______ = await _.get(`https://api.github.com/users/${process.env.GITHUB_ACTOR}/events`);
 
-  await ___.add('./*');
+  // await ___.add('./*');
   await ___.addConfig('user.name', process.env.GITHUB_ACTOR);
   await ___.addConfig('user.email', ______.data[0].payload.commits[0].author.email);
   await ___.commit(_____.data.replace(/(\r\n|\n|\r)/gm, ''), ['--amend']);
