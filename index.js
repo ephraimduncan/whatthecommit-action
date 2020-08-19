52b761063b016e392fbf3b5fddaf2a3d805c1396
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-19 11:34:10 +0000

    removed add.. you won't even see it

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const ___ = __();
 async function ____() {
   const _____ = await _.get('http://whatthecommit.com/index.txt');
   const ______ = await _.get(`https://api.github.com/users/${process.env.GITHUB_ACTOR}/events`);
-
-  // await ___.add('./*');
   await ___.addConfig('user.name', process.env.GITHUB_ACTOR);
   await ___.addConfig('user.email', ______.data[0].payload.commits[0].author.email);
   await ___.commit(_____.data.replace(/(\r\n|\n|\r)/gm, ''), ['--amend']);
