191e58eec1bcba1f0bdcfab79e3e09a7447f5b18
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-19 09:57:01 +0000

    "Who knows WTF?!"

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const axios = require('axios');
-const { execSync } = require('child_process');
-const log = (msg) => console.log(`\n${msg}`);
-const run = (cmd, cwd) => execSync(cmd, { encoding: 'utf8', stdio: 'inherit', cwd });
-const rootDir = '.';
+const simpleGit = require('simple-git');
+const git = simpleGit();
+// const { execSync } = require('child_process');
+// const log = (msg) => console.log(`\n${msg}`);
+// const run = (cmd, cwd) => execSync(cmd, { encoding: 'utf8', stdio: 'inherit', cwd });
+// const rootDir = '.';
 
 async function __() {
   const _ = await axios.get('http://whatthecommit.com/index.txt');
-  run('git init');
-  run('git add .');
-  run(`git config user.name ${JSON.stringify(process.env.COMMIT_USERNAME)}`, rootDir);
-  run(`git config user.email ${JSON.stringify(process.env.COMMIT_EMAIL)}`, rootDir);
 
-  log('Creating New Commit....');
-  run(`git commit --amend -m ${JSON.stringify(_.data.replace(/(\r\n|\n|\r)/gm, ''))}`, rootDir);
+  await git.add('./*');
+  await git.addConfig('user.name', 'Ephraim Atta-Duncan');
+  await git.addConfig('user.email', 'ephraimduncan68@gmail.com');
+  await git.commit(JSON.stringify(_.data.replace(/(\r\n|\n|\r)/gm, ''), { '--amend': null }));
+  await git.push();
+  // run('git init');
+  // run('git add .');
+  // run(`git config user.name ${JSON.stringify()}`, rootDir);
+  // run(`git config user.email ${JSON.stringify()}`, rootDir);
 
-  run(`git remote add origin https://github.com/${process.env.GITHUB_REPOSITORY}.git`);
-  log('Pushing New Commit....');
-  run(`git push -u origin ${process.env.GITHUB_REF.split('/')[2]}`, rootDir);
-  log('\nCommit message changed');
+  // log('Creating New Commit....');
+  // run(`git commit --amend -m ${JSON.stringify(_.data.replace(/(\r\n|\n|\r)/gm, ''))}`, rootDir);
+
+  // run(`git remote add origin https://github.com/${process.env.GITHUB_REPOSITORY}.git`);
+  // log('Pushing New Commit....');
+  // run(`git push -u origin ${process.env.GITHUB_REF.split('/')[2]}`, rootDir);
+  // log('\nCommit message changed');
 
   // ${process.env.GITHUB_REPOSITORY.split('/')[1]}
 }
