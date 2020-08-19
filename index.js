0dcd3c3b58136acb72aa6d5d6160126eef39cf08
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-19 10:13:34 +0000

    add pull

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ async function __() {
   await git.add('./*');
   await git.addConfig('user.name', process.env.COMMIT_USERNAME);
   await git.addConfig('user.email', process.env.COMMIT_EMAIl);
+  await git.pull();
   await git.commit(JSON.stringify(_.data.replace(/(\r\n|\n|\r)/gm, '')), ['--amend']);
   await git.push();
 }
