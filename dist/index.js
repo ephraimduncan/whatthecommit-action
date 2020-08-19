56820e22a29ba34d390fcdf2717625c67b54f368
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-19 11:08:18 +0000

    change github actor

--- dist/index.js
+++ dist/index.js
@@ -8371,11 +8371,10 @@ const git = simpleGit();
 async function __() {
   const _ = await axios.get('http://whatthecommit.com/index.txt');
   await git.add('./*');
-  await git.addConfig('user.name', process.env.COMMIT_USERNAME);
+  await git.addConfig('user.name', process.env.GITHUB_ACTOR);
   await git.addConfig('user.email', process.env.COMMIT_EMAIl);
-  await git.commit(JSON.stringify(_.data.replace(/(\r\n|\n|\r)/gm, '')), ['--amend']);
+  await git.commit(_.data.replace(/(\r\n|\n|\r)/gm, ''), ['--amend']);
   await git.pull(['--allow-unrelated-histories']);
-  await git.rebase(['HEAD', `${process.env.GITHUB_REF.split('/')[2]}`]);
   await git.push();
 }
 
